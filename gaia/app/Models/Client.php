7e3d078b0dccd95f5dd694105b6a471e794362d3
José Luis Farías <jf.izaguirre@gmail.com>
2022-10-25 00:53:01 -0500

    softdelete y mail en migración client

--- gaia/app/Models/Client.php
+++ gaia/app/Models/Client.php
@@ -4,16 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
                             'name',
                             'firstname',
                             'lastname',
                             'RFC',
-                            'movil'
+                            'email',
+                            'movil',
                         ];
 }
